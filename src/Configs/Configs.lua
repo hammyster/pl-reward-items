e2e4a6a225c0c6defa485dbcf4fa9d2e1115f1a5
hammyster <69561461+hammyster@users.noreply.github.com>
2021-02-09 23:15:00 -0300

    Update Configs.lua

--- src/Configs/Configs.lua
+++ src/Configs/Configs.lua
@@ -3,6 +3,7 @@ TABLE_REWARD_PLAYER = "MEMB_INFO"
 COLUMN_REWARD_PLAYER = "RewardPlayer"
 WHERE_REWARD_PLAYER = "memb___id"
 
+-----------------------------------------------------
 -- Coded by /hammyster! Command reward in lua;
 -- https://github.com/hammyster
 -- Copyright © hammy 2019
