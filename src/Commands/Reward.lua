19f0b3c9b2eb4b69fbc1465060b6931128cb240d
hammyster <69561461+hammyster@users.noreply.github.com>
2021-02-09 23:15:19 -0300

    Update Reward.lua

--- src/Commands/Reward.lua
+++ src/Commands/Reward.lua
@@ -77,10 +77,8 @@ Commands.Register(REWARD_COMMAND, Reward.GetReward)
 
 return Reward
 
+-----------------------------------------------------
 -- Coded by /hammyster! Command reward in lua;
 -- https://github.com/hammyster
 -- Copyright © hammy 2019
--- Licensed under a MIT license
--- Read the terms of the license here
--- https://github.com/Roshly/AyyHook-Loader/blob/master/LICENSE
 -----------------------------------------------------
