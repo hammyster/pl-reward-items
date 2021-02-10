410ce92d32f0d82de744eaa2fea57d97e6cfe0d2
hammyster <69561461+hammyster@users.noreply.github.com>
2021-02-09 23:14:48 -0300

    Update Configs.lua

--- src/Configs/Configs.lua
+++ src/Configs/Configs.lua
@@ -6,7 +6,4 @@ WHERE_REWARD_PLAYER = "memb___id"
 -- Coded by /hammyster! Command reward in lua;
 -- https://github.com/hammyster
 -- Copyright © hammy 2019
--- Licensed under a MIT license
--- Read the terms of the license here
--- https://github.com/Roshly/AyyHook-Loader/blob/master/LICENSE
 -----------------------------------------------------
